113a2b31acb286346980b517f717b6f428ffb8cf
Travis Overby <tek62441@adobe.com>
2019-06-14 16:33:02 -0700

    Add new Navigation sub-components

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,11 @@
-import React from 'React';
+import React from 'react';
 
 import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css';
 
 const logo = (props) => (
   <div className={classes.Logo}>
-    <img src={burgerLogo} alt="MyBurger" />
+    <img src={burgerLogo} alt="MyBurger"/>
   </div>
 );
 
